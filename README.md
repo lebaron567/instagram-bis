a8941a5ec96a0d3570737fb5ab78ca2fea4ef46b
steven04458 <steven.spein2@gmail.com>
2024-12-05 14:14:50 +0100

    fichier config

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 go mod init instagram-bis
 
 go mod tidy
+
+go get gorm.io/driver/sqlite
--- config/config.go
+++ config/config.go
@@ -1,22 +1,61 @@
 package config
 
+import (
+	"log"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 type Config struct {
-    // Connexion aux repositories
-
+	DB                       *gorm.DB
+	authenticationRepository dbmodel.authenticationRepository
+	userRepository           dbmodel.userRepository
+	postRepository           dbmodel.postRepository
+	commentRepository        dbmodel.commentRepository
+	likeRepository           dbmodel.likeRepository
+	followRepository         dbmodel.followRepository
+	notificationRepository   dbmodel.notificationRepository
+	messageRepository        dbmodel.messageRepository
+	discussionRepository     dbmodel.discussionRepository
+	membersRepository        dbmodel.membersRepository
 }
 
 func New() (*Config, error) {
-    config := Config{}
+	db, err := gorm.Open(sqlite.Open("intagrambis.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Impossible de se connecter à la base de données : %v", err)
+		return nil, err
+	}
+	if err := db.AutoMigrate(&dbmodel.User{}, &dbmodel.Post{}, &dbmodel.Comment{}, &dbmodel.Like{}, &dbmodel.Follow{}, &dbmodel.Notification{}, &dbmodel.Message{}, &dbmodel.Discussion{}, &dbmodel.Members{}); err != nil {
+		log.Fatal("Erreur lors de la migration : %v", err)
+		return nil, err
+	}
 
-    // Initialisation de la connexion à la base de données
+	authenticationRepo := dbmodel.NewAuthenticationRepository(db)
+	userRepo := dbmodel.NewUserRepository(db)
+	postRepo := dbmodel.NewPostRepository(db)
+	commentRepo := dbmodel.NewCommentRepository(db)
+	likeRepo := dbmodel.NewLikeRepository(db)
+	followRepo := dbmodel.NewFollowRepository(db)
+	notificationRepo := dbmodel.NewNotificationRepository(db)
+	messageRepo := dbmodel.NewMessageRepository(db)
+	discussionRepo := dbmodel.NewDiscussionRepository(db)
+	membersRepo := dbmodel.NewMembersRepository(db)
 
-    // Migration des modèles
+	config := Config{
+		DB:                       db,
+		authenticationRepository: authenticationRepo,
+		userRepository:           userRepo,
+		postRepository:           postRepo,
+		commentRepository:        commentRepo,
+		likeRepository:           likeRepo,
+		followRepository:         followRepo,
+		notificationRepository:   notificationRepo,
+		messageRepository:        messageRepo,
+		discussionRepository:     discussionRepo,
+		membersRepository:        membersRepo,
+	}
 
-
-    // Initialisation des repositories
-
-
-    return &config, nil
+	return &config, nil
 }
