5d0deb83ef3256b51477941f78d363e3b6fde851
emeric barotin <emericbarotin@gmail.com>
2024-12-05 15:28:54 +0100

    work in progrese

--- pkg/comment/routes.go
+++ pkg/comment/routes.go
@@ -1,1 +1,16 @@
 package comment
+
+
+import (
+    "github.com/go-chi/chi/v5"
+    "net/http"
+)
+
+func Routes() chi.Router {
+    router := chi.NewRouter()
+    router.Post("/posts/:id/comments",r)
+    router.Get("/posts/:id/comments", )
+	router.Delete("/comments/:id,", )
+    return router
+}
+
