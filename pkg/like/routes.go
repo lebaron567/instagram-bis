bcaa2fd512ac3ed29c38a9f80d4ff220e0af1464
emeric barotin <emericbarotin@gmail.com>
2025-01-05 22:52:51 +0100

    :white_check_mark:| add controller et route

--- pkg/like/routes.go
+++ pkg/like/routes.go
@@ -1,1 +1,15 @@
 package like
+
+import (
+    "github.com/go-chi/chi/v5"
+    "instagram-bis/config"
+)
+
+func Routes(cfg *config.Config) chi.Router {
+    r := chi.NewRouter()
+
+    r.Post("/posts/{id}/like", LikePost(cfg))
+    r.Delete("/posts/{id}/like", UnlikePost(cfg))
+
+    return r
+}
