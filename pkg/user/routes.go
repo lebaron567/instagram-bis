ace1482430f1395b08183cc7c4b7745fda1d9120
emeric barotin <emericbarotin@gmail.com>
2025-01-05 22:05:32 +0100

    :construction:| add roude et controller user

--- pkg/user/routes.go
+++ pkg/user/routes.go
@@ -1,1 +1,20 @@
 package user
+
+import (
+    "github.com/go-chi/chi/v5"
+    "instagram-bis/config"
+)
+
+func Routes(cfg *config.Config) chi.Router {
+    r := chi.NewRouter()
+
+    r.Post("/users/register", RegisterUser(cfg))
+    r.Post("/users/login", LoginUser(cfg))
+    r.Get("/users/{id}", GetUserProfile(cfg))
+    r.Put("/users/{id}", UpdateUserProfile(cfg))
+    r.Post("/users/{id}/follow", FollowUser(cfg))
+    r.Get("/users/{id}/followers", GetFollowers(cfg))
+    r.Get("/users/{id}/following", GetFollowing(cfg))
+
+    return r
+}
