2dcf0fef1b5fa403904c6ec96a612bd31383569d
arthurLacombe <arthurlacombe44@gmail.com>
2025-01-06 08:54:30 +0100

    change dbmodel to add an update function

--- database/dbmodel/messages.go
+++ database/dbmodel/messages.go
@@ -17,6 +17,7 @@ type MessageRepository interface {
 	Create(message *Message) (*Message, error)
 	FindByDiscussionID(discussionID int) ([]*Message, error)
 	Delete(id int) error
+	Update(id int, updatedMessage *Message) (*Message, error) // Nouvelle méthode pour la mise à jour
 }
 
 type messageRepository struct {
@@ -40,6 +41,24 @@ func (r *messageRepository) FindByDiscussionID(discussionID int) ([]*Message, er
 		return nil, err
 	}
 	return messages, nil
+}
+
+func (r *messageRepository) Update(id int, updatedMessage *Message) (*Message, error) {
+	// Rechercher le message existant
+	var existingMessage Message
+	if err := r.db.First(&existingMessage, id).Error; err != nil {
+		return nil, err // Retourne une erreur si le message n'existe pas
+	}
+
+	// Mettre à jour les champs du message
+	existingMessage.Content = updatedMessage.Content
+
+	// Sauvegarder les modifications
+	if err := r.db.Save(&existingMessage).Error; err != nil {
+		return nil, err
+	}
+
+	return &existingMessage, nil
 }
 
 func (r *messageRepository) Delete(id int) error {
